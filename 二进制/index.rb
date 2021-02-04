b05466a6c30f04b48ebec6bd3c836e85f597204d
messiahxu <messiahxu@gmail.com>
2021-02-04 14:09:57 +0800

    二进制转十进制

--- 二进制/index.rb
+++ 二进制/index.rb
@@ -22,3 +22,20 @@ def test_int_to_bin(num)
   puts "our_binary_string = #{our_binary_string}"
   puts "ruby_binary_string = #{ruby_binary_string}"
 end
+
+# 二进制转十进制方法
+# 不知道怎么解释，举例说明
+# 二进制数 100 的第一位，
+# 二进制数从右向左数位数，第一位是 0
+# 以此类推，每一位的对应十进制数就是某一位的值 * 2的位数次幂，如第三位是1 即 1 * 2 ** 3，各位相加即可
+
+def bin_to_int(bin_string)
+  bin_string.split(//u).reverse.map.with_index{|e, index| e.to_i * (2 ** index) }.inject(:+)
+end
+
+def test_bin_to_int(bin_string)
+  our_int = bin_to_int(bin_string)
+  ruby_int = eval('0b'+bin_string)
+  puts "our_int = #{our_int}"
+  puts "ruby_int = #{ruby_int}"
+end
